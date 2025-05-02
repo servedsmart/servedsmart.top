e3196bbc9df94a9b04d7366b023a7523f0dc7bd5
Served Smart <195884188+servedsmart@users.noreply.github.com>
2025-05-02 23:42:36 +0200

    Use script to generate favicons and modify compression

---
+++ graphics/projects/logo/favicon.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+###
+# File: favicon.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2025 Leopold Meinel & contributors
+# SPDX ID: MIT
+# URL: https://opensource.org/licenses/MIT
+# -----
+###
+
+# Fail on error
+set -e
+
+# Source config
+SCRIPT_DIR="$(dirname -- "$(readlink -f -- "${0}")")"
+
+INPUT="${SCRIPT_DIR}"/logo.svg
+OUTPUT_DIR="${SCRIPT_DIR}"/favicon
+# These will only be exported as png
+SIZES=(
+    16
+    32
+    180
+    192
+    512
+)
+# These will also be included in .ico
+SIZES_ICO="16,32,48"
+
+mkdir -p "${OUTPUT_DIR}"
+
+for size in "${SIZES[@]}"; do
+    if [[ "${size}" -eq 512 ]] || [[ "${size}" -eq 192 ]]; then
+        FILES+=("${OUTPUT_DIR}"/android-chrome-"${size}"x"${size}".png)
+        continue
+    fi
+    if [[ "${size}" -eq 180 ]]; then
+        FILES+=("${OUTPUT_DIR}"/apple-touch-icon.png)
+        continue
+    fi
+    FILES+=("${OUTPUT_DIR}"/favicon-"${size}"x"${size}".png)
+done
+
+SIZES_LENGTH="${#SIZES[@]}"
+for ((i = 0; i < SIZES_LENGTH; i++)); do
+    SIZE="${SIZES[${i}]}"
+    OUTPUT="${FILES[${i}]}"
+    inkscape "${INPUT}" -h "${SIZE}" -w "${SIZE}" -o "${OUTPUT}"
+    oxipng -a -s -o 4 "${OUTPUT}"
+done
+
+magick -background transparent "${INPUT}" -define icon:auto-resize="${SIZES_ICO}" "${OUTPUT_DIR}"/favicon.ico
