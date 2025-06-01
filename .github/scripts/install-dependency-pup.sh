63312518940d7787b8b3f658c91d15b68a8c3c31
Served Smart <195884188+servedsmart@users.noreply.github.com>
2025-06-01 04:19:04 +0200

    Use sudo to move pup binary

--- .github/scripts/install-dependency-pup.sh
+++ .github/scripts/install-dependency-pup.sh
@@ -15,4 +15,5 @@ set -e
 REPO_DIR="$(mktemp -d /tmp/pup-XXXXXX)"
 git clone https://github.com/servedsmart/pup "${REPO_DIR}"
 cd "${REPO_DIR}"
-go build -o /usr/bin/pup
+go build -o /tmp/pup
+sudo mv /tmp/pup /usr/bin/pup
