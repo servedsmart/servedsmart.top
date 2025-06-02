f593e6d97f2ce99c495e0bf2bc1df8f40d8a262b
Served Smart <195884188+servedsmart@users.noreply.github.com>
2025-06-02 10:45:00 +0200

    Add manifest-src 'self';

--- .github/scripts/deploy-cloudflare-api.sh
+++ .github/scripts/deploy-cloudflare-api.sh
@@ -149,6 +149,7 @@ for target_branch in "${TARGET_BRANCHES[@]}"; do
         "child-src 'self' https://www.youtube-nocookie.com"
         "form-action 'self'"
         "frame-ancestors 'none'"
+        "manifest-src 'self';"
         "base-uri 'self'"
         "upgrade-insecure-requests"
     )
@@ -175,6 +176,7 @@ RULES_CSP_CMS=(
     "child-src blob: https://www.youtube-nocookie.com"
     "form-action 'self'"
     "frame-ancestors 'none'"
+    "manifest-src 'self';"
     "base-uri 'self'"
     "upgrade-insecure-requests"
     "font-src 'self' https://fonts.gstatic.com"
